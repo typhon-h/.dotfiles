e8a7dc18a612ead5e3e4aa2ea1e0b489cf44dbc7
Harrison Tyson <hty13@uclive.ac.nz>
2024-05-22 01:52:09 +1200

    Add telescope

---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,12 @@
+return {
+  'nvim-telescope/telescope.nvim',
+  tag = '0.1.6',
+  dependencies = { 'nvim-lua/plenary.nvim' },
+  config = function()
+    local builtin = require("telescope.builtin")
+    vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+    -- requires ripgrep installed
+    vim.keymap.set('n','<leader>fg', builtin.live_grep, {})
+  end
+}
+
