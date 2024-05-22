d820c0d80737896698cabfb5e1afdab0f5f8f456
Harrison Tyson <hty13@uclive.ac.nz>
2024-05-23 01:46:14 +1200

    Update diagnostics to show with telescope

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,51 +1,58 @@
 return {
-  {
-    "williamboman/mason.nvim",
-    config = function ()
-      require("mason").setup()
-    end
-  },
-  {
-    "williamboman/mason-lspconfig.nvim",
-    config = function ()
-      require("mason-lspconfig").setup({
-        ensure_installed = {
-          "lua_ls",
-          "clangd",
-          "html",
-          "jdtls",
-          "tsserver",
-          "kotlin_language_server",
-          "pyright",
-          "omnisharp",
-          "volar"
-        }
-      })
-    end
-  },
-  {
-    "neovim/nvim-lspconfig",
-    config = function ()
-      local lspconfig = require("lspconfig")
-      lspconfig.lua_ls.setup({})
-      lspconfig.clangd.setup({})
-      lspconfig.html.setup({})
-      lspconfig.jdtls.setup({})
-      lspconfig.tsserver.setup({})
-      lspconfig.kotlin_language_server.setup({})
-      lspconfig.pyright.setup({})
-      lspconfig.omnisharp.setup({})
-      lspconfig.volar.setup({})
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = {
+					"lua_ls",
+					"clangd",
+					"html",
+					"jdtls",
+					"tsserver",
+					"kotlin_language_server",
+					"pyright",
+					"omnisharp",
+					"volar",
+				},
+			})
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			local lspconfig = require("lspconfig")
+			lspconfig.lua_ls.setup({})
+			lspconfig.clangd.setup({})
+			lspconfig.html.setup({})
+			lspconfig.jdtls.setup({})
+			lspconfig.tsserver.setup({})
+			lspconfig.pyright.setup({})
+			lspconfig.omnisharp.setup({})
+			lspconfig.volar.setup({})
 
-      vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
-      vim.keymap.set({'n', 'v'}, '<leader>ca', vim.lsp.buf.code_action, {})
-      vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, {})
+			vim.diagnostic.config({
+				virtual_text = false,
+			})
 
-      local telescope = require("telescope.builtin")
-      vim.keymap.set('n', '<leader>gd', telescope.lsp_definitions, {})
-      vim.keymap.set('n', '<leader>gi', telescope.lsp_implementations, {})
-      vim.keymap.set('n', '<leader>gr', telescope.lsp_references, {})
+			-- Show line diagnostics automatically in hover window
+			vim.o.updatetime = 250
+			vim.cmd([[autocmd CursorHold,CursorHoldI * lua vim.diagnostic.open_float(nil, {focus=false})]])
 
-    end
-  }
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+			vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, {})
+
+			local telescope = require("telescope.builtin")
+			vim.keymap.set("n", "<leader>gd", telescope.lsp_definitions, {})
+			vim.keymap.set("n", "<leader>gi", telescope.lsp_implementations, {})
+			vim.keymap.set("n", "<leader>gr", telescope.lsp_references, {})
+      vim.keymap.set("n", "<leader>d", telescope.diagnostics, {})
+		end,
+	},
 }
