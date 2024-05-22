798f2bb923fd661887b4f0be30c29e95389df872
Harrison Tyson <hty13@uclive.ac.nz>
2024-05-22 23:29:23 +1200

    add base lsp config

---
+++ lua/plugins/lsp-config.lua
@@ -0,0 +1,51 @@
+return {
+  {
+    "williamboman/mason.nvim",
+    config = function ()
+      require("mason").setup()
+    end
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    config = function ()
+      require("mason-lspconfig").setup({
+        ensure_installed = {
+          "lua_ls",
+          "clangd",
+          "html",
+          "jdtls",
+          "tsserver",
+          "kotlin_language_server",
+          "pyright",
+          "omnisharp",
+          "volar"
+        }
+      })
+    end
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function ()
+      local lspconfig = require("lspconfig")
+      lspconfig.lua_ls.setup({})
+      lspconfig.clangd.setup({})
+      lspconfig.html.setup({})
+      lspconfig.jdtls.setup({})
+      lspconfig.tsserver.setup({})
+      lspconfig.kotlin_language_server.setup({})
+      lspconfig.pyright.setup({})
+      lspconfig.omnisharp.setup({})
+      lspconfig.volar.setup({})
+
+      vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
+      vim.keymap.set({'n', 'v'}, '<leader>ca', vim.lsp.buf.code_action, {})
+      vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, {})
+
+      local telescope = require("telescope.builtin")
+      vim.keymap.set('n', '<leader>gd', telescope.lsp_definitions, {})
+      vim.keymap.set('n', '<leader>gi', telescope.lsp_implementations, {})
+      vim.keymap.set('n', '<leader>gr', telescope.lsp_references, {})
+
+    end
+  }
+}
