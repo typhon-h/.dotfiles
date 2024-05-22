2a6822cfba343379a8ce5aa2cf72fae87b4521c1
Harrison Tyson <hty13@uclive.ac.nz>
2024-05-23 02:16:52 +1200

    add initial snippet completion functionality. remove diagnostic hover window

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -36,16 +36,6 @@ return {
 			lspconfig.omnisharp.setup({})
 			lspconfig.volar.setup({})
 
-			vim.diagnostic.config({
-				virtual_text = false,
-			})
-
-			-- Show line diagnostics automatically in hover window
-			vim.o.updatetime = 250
-			vim.cmd([[autocmd CursorHold,CursorHoldI * lua vim.diagnostic.open_float(nil, {focus=false})]])
-
-			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
 			vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, {})
 
 			local telescope = require("telescope.builtin")
