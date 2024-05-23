715fa4bffe05c63d0be832110fa75c0dab27c512
Harrison Tyson <hty13@uclive.ac.nz>
2024-05-23 23:23:44 +1200

    add basic debug config

---
+++ lua/plugins/debugger.lua
@@ -0,0 +1,36 @@
+return {
+	"mfussenegger/nvim-dap",
+	dependencies = {
+		"rcarriga/nvim-dap-ui",
+		"nvim-neotest/nvim-nio",
+		"mfussenegger/nvim-dap-python",
+
+	},
+	config = function()
+		local dap = require("dap")
+		local dapui = require("dapui")
+		dapui.setup({})
+
+		local path = require("mason-registry").get_package("debugpy"):get_install_path()
+		require("dap-python").setup(path .. "/venv/bin/python")
+
+		dap.listeners.before.attach.dapui_config = function()
+			dapui.open()
+		end
+		dap.listeners.before.launch.dapui_config = function()
+			dapui.open()
+		end
+		dap.listeners.before.event_terminated.dapui_config = function()
+			dapui.close()
+		end
+		dap.listeners.before.event_exited.dapui_config = function()
+			dapui.close()
+		end
+
+		vim.keymap.set("n", "<leader>db", dap.toggle_breakpoint, {})
+		vim.keymap.set("n", "<leader>dc", dap.continue, {})
+		vim.keymap.set("n", "<leader>dd", dap.step_over, {})
+		vim.keymap.set("n", "<leader>di", dap.step_into, {})
+		vim.keymap.set("n", "<leader>do", dap.step_out, {})
+	end,
+}
