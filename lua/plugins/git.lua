6e5c7b0bc6b7a2ec82041f31481f62c335e4ba73
Harrison Tyson <hty13@uclive.ac.nz>
2024-05-24 02:36:18 +1200

    Add basic git integration

---
+++ lua/plugins/git.lua
@@ -0,0 +1,19 @@
+return {
+	{
+		"tpope/vim-fugitive",
+	},
+	{
+		"lewis6991/gitsigns.nvim",
+
+		config = function()
+			require("gitsigns").setup()
+			local telescope = require("telescope.builtin")
+
+			vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", {})
+            vim.keymap.set("n", "<leader>gb", telescope.git_branches, {})
+            vim.keymap.set("n", "<leader>gs", ":Gitsigns stage_hunk<CR>", {})
+            vim.keymap.set("n", "<leader>gu", ":Gitsigns undo_stage_hunk<CR>", {})
+            vim.keymap.set("n", "<leader>gc", telescope.git_commits, {})
+		end,
+	},
+}
