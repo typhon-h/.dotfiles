1a505ebb444a3a4a6c5411be5dc76ed6f1e14f48
Harrison Tyson <hty13@uclive.ac.nz>
2024-05-24 20:33:41 +1200

    add harpoon

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -7,7 +7,7 @@ return {
     "MunifTanjim/nui.nvim",
   },
   config = function()
-    vim.keymap.set('n', '<C-n>', ':Neotree filesystem reveal float<CR>', {})
+    vim.keymap.set('n', '<leader>n', ':Neotree filesystem reveal float<CR>', {})
   end
 }
 
