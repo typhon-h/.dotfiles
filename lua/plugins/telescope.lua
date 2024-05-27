4b1cc5f202f907634c4c1e6ab3bd9eab12ffed3a
Harrison Tyson <hty13@uclive.ac.nz>
2024-05-28 01:00:28 +1200

    change diagnostic window to horizontal

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,40 +1,46 @@
 return {
-	{
-		"nvim-telescope/telescope.nvim",
-		tag = "0.1.6",
-		dependencies = { "nvim-lua/plenary.nvim" },
-		config = function()
-			require("telescope").setup({
-				defaults = {
-					prompt_prefix = " ",
-					selection_caret = " ",
-					path_display = { "smart" },
-					winblend = 10,
-					layout_strategy = "flex",
-					file_ignore_patterns = {
-						"node_modules",
-						"build",
-					},
-				},
-			})
-			local builtin = require("telescope.builtin")
+    {
+        "nvim-telescope/telescope.nvim",
+        tag = "0.1.6",
+        dependencies = { "nvim-lua/plenary.nvim" },
+        config = function()
+            require("telescope").setup({
+                defaults = {
+                    prompt_prefix = " ",
+                    selection_caret = " ",
+                    path_display = { "smart" },
+                    winblend = 10,
+                    layout_strategy = "flex",
+                    file_ignore_patterns = {
+                        "node_modules",
+                        "build",
+                    },
+                },
+                pickers = {
+                    diagnostics = {
+                        theme = "dropdown",
+                        layout_strategy = "vertical",
+                    },
+                },
+            })
+            local builtin = require("telescope.builtin")
 
-			vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
-			-- requires ripgrep installed
-			vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
-		end,
-	},
-	{
-		"nvim-telescope/telescope-ui-select.nvim",
-		config = function()
-			require("telescope").setup({
-				extensions = {
-					["ui-select"] = {
-						require("telescope.themes").get_dropdown({}),
-					},
-				},
-			})
-			require("telescope").load_extension("ui-select")
-		end,
-	},
+            vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
+            -- requires ripgrep installed
+            vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+        end,
+    },
+    {
+        "nvim-telescope/telescope-ui-select.nvim",
+        config = function()
+            require("telescope").setup({
+                extensions = {
+                    ["ui-select"] = {
+                        require("telescope.themes").get_dropdown({}),
+                    },
+                },
+            })
+            require("telescope").load_extension("ui-select")
+        end,
+    },
 }
