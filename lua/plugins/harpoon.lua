07ac9847b2de18edb518981df82b3f0c2c05993e
Harrison Tyson <hty13@uclive.ac.nz>
2024-06-02 20:41:33 +1200

    modify harpoon to use quick menu instead of telescope

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -1,69 +1,38 @@
 return {
-	"ThePrimeagen/harpoon",
+    "ThePrimeagen/harpoon",
     branch = "harpoon2",
-	dependencies = { "nvim-lua/plenary.nvim" },
-	config = function()
-		local harpoon = require("harpoon")
-		local conf = require("telescope.config").values
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+        local harpoon = require("harpoon")
 
-		local function toggle_telescope(harpoon_files)
-			local finder = function()
-				local paths = {}
-				for _, item in ipairs(harpoon_files.items) do
-					table.insert(paths, item.value)
-				end
-
-				return require("telescope.finders").new_table({
-					results = paths,
-				})
-			end
-
-			require("telescope.pickers")
-				.new({}, {
-					prompt_title = "Harpoon",
-					finder = finder(),
-					previewer = conf.file_previewer({}),
-					sorter = conf.generic_sorter({}),
-					attach_mappings = function(prompt_bufnr, map)
-						map("i", "<C-d>", function()
-							local state = require("telescope.actions.state")
-							local selected_entry = state.get_selected_entry()
-							local current_picker = state.get_current_picker(prompt_bufnr)
-
-							table.remove(harpoon_files.items, selected_entry.index)
-							current_picker:refresh(finder())
-						end)
-						return true
-					end,
-				})
-				:find()
-		end
-
-		vim.keymap.set("n", "<leader>a", function()
-			harpoon:list():add()
-			vim.notify("Current buffer added to Harpoon", vim.log.levels.INFO)
-		end)
+        vim.keymap.set("n", "<leader>a", function()
+            harpoon:list():add()
+            vim.notify("Current buffer added to Harpoon", vim.log.levels.INFO)
+        end)
 
         vim.keymap.set("n", "<leader>r", function()
             harpoon:list():clear()
             vim.notify("Harpoon reset", vim.log.levels.INFO)
         end)
 
-		vim.keymap.set("n", "<leader>h", function()
-			toggle_telescope(harpoon:list())
-		end, { desc = "Open harpoon window" })
+        vim.keymap.set("n", "<leader>h", function()
+            harpoon.ui:toggle_quick_menu(harpoon:list())
+        end)
 
-		vim.keymap.set("n", "<C-h>", function()
-			harpoon:list():select(1)
-		end)
-		vim.keymap.set("n", "<C-t>", function()
-			harpoon:list():select(2)
-		end)
-		vim.keymap.set("n", "<C-n>", function()
-			harpoon:list():select(3)
-		end)
-		vim.keymap.set("n", "<C-s>", function()
-			harpoon:list():select(4)
-		end)
-	end,
+        vim.keymap.set("n", "<C-h>", function()
+            harpoon:list():select(1)
+        end)
+
+        vim.keymap.set("n", "<C-t>", function()
+            harpoon:list():select(2)
+        end)
+
+        vim.keymap.set("n", "<C-n>", function()
+            harpoon:list():select(3)
+        end)
+
+        vim.keymap.set("n", "<C-s>", function()
+            harpoon:list():select(4)
+        end)
+    end,
 }
