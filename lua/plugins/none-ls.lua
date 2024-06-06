3c860e2e30d8d9b1cd876762754fac02b99c2caa
Harrison Tyson <hty13@uclive.ac.nz>
2024-06-07 01:35:35 +1200

    add checkstyle and java formatter

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,22 +1,25 @@
 return {
-  "nvimtools/none-ls.nvim",
-      dependencies = {
-      "nvimtools/none-ls-extras.nvim",
+    "nvimtools/none-ls.nvim",
+    dependencies = {
+        "nvimtools/none-ls-extras.nvim",
     },
-  config = function()
-    local null_ls = require("null-ls")
-    null_ls.setup({
-      sources = {
-        null_ls.builtins.formatting.stylua,
-        null_ls.builtins.formatting.prettier,
-        null_ls.builtins.formatting.black,
-        null_ls.builtins.formatting.isort,
+    config = function()
+        local null_ls = require("null-ls")
+        null_ls.setup({
+            sources = {
+                null_ls.builtins.formatting.stylua,
+                null_ls.builtins.formatting.prettier,
+                null_ls.builtins.formatting.black,
+                null_ls.builtins.formatting.isort,
+                null_ls.builtins.formatting.google_java_format,
 
-        null_ls.builtins.diagnostics.semgrep,
-        require("none-ls.diagnostics.eslint_d"),
-      },
-    })
+                null_ls.builtins.diagnostics.checkstyle.with({
+                    extra_args = { "-c", "/google_checks.xml" },
+                }),
+                require("none-ls.diagnostics.eslint_d"),
+            },
+        })
 
-    vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
-  end,
+        vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+    end,
 }
