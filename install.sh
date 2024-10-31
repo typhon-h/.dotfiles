36667dd7e12433c428885f91833cf6d3041fc130
Harrison Tyson <hty13@uclive.ac.nz>
2024-11-01 00:58:15 +1300

    install script for linux, mostly works on osx

---
+++ install.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Install required packages
+sudo apt-get -y --ignore-missing install $(< packages.list)
+
+
+# Starship
+curl -sS https://starship.rs/install.sh | sh
+
+
+# Kitty
+curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin
+
+
+# Nerd Font
+curl -L -o ~/.local/share/fonts/JetBrainsMono.zip https://github.com/ryanoasis/nerd-fonts/releases/latest/download/JetBrainsMono.zip
+unzip ~/.local/share/fonts/JetBrainsMono.zip -d ~/.local/share/fonts/
+rm ~/.local/share/fonts/JetBrainsMono.zip
+fc-cache -fv
+
+
+# Setup config symlinks
+mv .stow-global-ignore ~/.stow-global-ignore
+stow -v -R --adopt -t $HOME */
+git restore .
+
+# Source the relevant rc file if possible
+rc_file="$HOME/.${SHELL##*/}rc"
+if [ -f "$rc_file" ]; then
+    source "$rc_file"
+fi
