3bfe4e14cfa728b75c9462049c9d1a34a66eee8c
Harrison Tyson <harrison.tyson1@gmail.com>
2024-11-01 17:39:02 +1300

    make kitty visible in desktop and default on install

--- install.sh
+++ install.sh
@@ -15,6 +15,10 @@ curl -sS https://starship.rs/install.sh | sh -s -- --yes
 
 # Kitty
 curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh -s -- launch=n
+ln -sf ~/.local/kitty.app/bin/kitty ~/.local/kitty.app/bin/kitten ~/.local/bin/
+cp ~/.local/kitty.app/share/applications/kitty.desktop ~/.local/share/applications/
+sed -i "s|Icon=kitty|Icon=$(readlink -f ~)/.local/kitty.app/share/icons/hicolor/256x256/apps/kitty.png|g" ~/.local/share/applications/kitty*.desktop
+sed -i "s|Exec=kitty|Exec=$(readlink -f ~)/.local/kitty.app/bin/kitty|g" ~/.local/share/applications/kitty*.desktop
 
 
 # Nerd Font
