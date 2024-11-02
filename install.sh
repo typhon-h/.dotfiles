1ebfe4afba83602ac82e5df12c41bf892f510951
Harrison Tyson <hty13@uclive.ac.nz>
2024-11-03 00:52:34 +1300

    remove sourcing at end of install

--- install.sh
+++ install.sh
@@ -13,11 +13,11 @@ else
 fi
 
 
-if [ -f "$HOME/.bashrc"]; then
+if [ -f "$HOME/.bashrc" ]; then
   mv "$HOME/.bashrc" "$HOME/.bashrc.backup"
 fi
 
-if [ -f "$HOME/.zshc"]; then
+if [ -f "$HOME/.zshc" ]; then
   mv "$HOME/.zshrc" "$HOME/.zshrc.backup"
 fi
 
@@ -26,13 +26,3 @@ fi
 ln -sf "$PWD/.stow-global-ignore" "$HOME/.stow-global-ignore"
 stow -v -R --adopt -t $HOME */
 git restore .
-
-
-# Check the current shell and source the relevant file
-current_shell=$(basename "$SHELL")
-
-if [ "$current_shell" = "bash" ]; then
-    source ~/.bashrc
-elif [ "$current_shell" = "zsh" ]; then
-    source ~/.zshrc
-fi
